44f5e4258c82c15bed839072a36337f18aced89e
Jose Cardona <jcardona321@gmail.com>
2023-08-09 21:38:06 -0600

    google A

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,8 @@
 import Nav from '@/components/NavComponent/Nav';
-import './globals.css';
 import Footer from '@/components/FooterComponent/Footer';
 import Script from 'next/script';
 import Link from 'next/link';
+import './globals.css';
 
 export const metadata = {
   title: 'Elite Fitness Gym',
@@ -12,13 +12,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={"gym-body"}>
-        <Nav />
-        <main style={{ minHeight: "80vh" }}>
-          {children}
-        </main>
-        <Footer />
-
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content={metadata.description} />
+        <title>{metadata.title}</title>
+        <link rel="stylesheet" href="path/to/your/stylesheet.css" />
         {/* Google Analytics */}
         {process.env.NEXT_PUBLIC_GA_ID && (
           <>
@@ -37,6 +36,23 @@ export default function RootLayout({ children }) {
             </Script>
           </>
         )}
+        {/* Your custom Google Analytics script */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-679TB9WYFR"></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-679TB9WYFR');
+          `}
+        </script>
+      </head>
+      <body className="gym-body">
+        <Nav />
+        <main style={{ minHeight: "80vh" }}>
+          {children}
+        </main>
+        <Footer />
       </body>
     </html>
   );
