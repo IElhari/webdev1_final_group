6a05787e5d9b98f3e0f012c035d7bafbf1db582c
Jose Cardona <jcardona321@gmail.com>
2023-08-09 20:00:26 -0600

    First Commit

---
+++ src/app/layout.js
@@ -0,0 +1,26 @@
+import Nav from '@/components/NavComponent/Nav'
+import './globals.css'
+import Footer from '@/components/FooterComponent/Footer'
+import Script from 'next/script'
+// import { Inter } from 'next/font/google'
+
+// const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Elite Fitness Gym',
+  description: 'Experience elite training and world-class gym facilities.',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={"gym-body"}>
+        <Nav /> 
+        <main style={{minHeight: "80vh"}}> 
+          {children}          
+        </main>
+        <Footer /> 
+      </body>
+    </html>
+  )
+}
